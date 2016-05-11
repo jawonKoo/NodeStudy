4141856025456d0d5f23c84abfda2411cc2e1fe8
KooJawon <ouners@naver.com>
2016-05-11 07:49:32 +0000

    update

--- meadowlark/site/meadowlark.js
+++ meadowlark/site/meadowlark.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
 var formidable = require('formidable');
+var credentials = require('./credentials.js');
+var jqupload = require('jquery-file-upload-middleware');
 
 var app = express();
+
+var VALID_EMAIL_REGEX = new RegExp(
+    '^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@' +
+    '[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?' +
+    '(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$'
+);
 
 var handlebars = require('express-handlebars').create({
     defaultLayout:'main',
@@ -24,12 +32,37 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(require('body-parser').urlencoded({ extneded: true }));
 
+app.use(require('cookie-parser')(credentials.cookieSecret));
+
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret
+}));
+
 app.use(function(req, res, next){
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
     if(!res.locals.partials) res.locals.partials = {};
     res.locals.partials.weatherContext = getWeatherData();
+
+    //플래시 메세지가 있다면 콘텍스트에 전달한 다음 지웁니다.
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
     next();
 });
+
+app.use('/upload', function(req, res, next){
+    var now = Date.now();
+    jqupload.fileHandler({
+        uploadDir: function(){
+            return __dirname + '/public/uploads/' + now;
+        },
+        uploadUrl: function(){
+            return '/uploads/' + now;
+        },
+    })(req, res, next);
+});
+
 
 app.get('/', function(req, res){
   res.render('home');
@@ -69,6 +102,39 @@ app.get('/tours/request-group-rate', function(req, res){
 
 app.get('/newsletter', function(req, res){
     res.render('newsletter', {csrf: 'CSRF token goes here'});
+});
+
+app.post('/newsletter', function(req, res){
+    var name = req.body.name || '', email = req.body.email || '';
+    //입력 유효성 검사
+    if(!email.match(VALID_EMAIL_REGEX)) {
+        if(req.xhr) return res.json({error: 'Invalid name email address.'});
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Validation error!',
+            message: 'The email address you entered was not valid.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    }
+
+    new NewsletterSignup({ name: name, email: email}).save(function(err){
+        if(err) {
+            if(req.xhr) return res.json({ error: 'Database error.'});
+            req.sesstion.flash = {
+                type: 'danger',
+                intro: 'Database error!',
+                message: 'There was a database error; please try agin later.',
+            };
+            return res.redirect(303, '/newsletter/archive');
+        }
+        if(req.xhr) return res.json({ success: true});
+        req.session.flash = {
+            type: 'success',
+            intro: 'Thank you!',
+            message: 'You have now been signed up for the newsletter.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    });
 });
 
 app.post('/process', function(req, res){
@@ -123,6 +189,18 @@ app.listen(app.get('port'), function(){
   console.log( 'Express started on http://kooserver.iptime.org:' + app.get('port') + '; press Ctrl + C to terminate.');
 });
 
+function NewsletterSignup(data){
+
+    function save(callback){
+        if(typeof callback === "function"){
+            callback(false);
+        }
+    }
+
+    return {
+        save : save,
+    };
+}
 
 function getWeatherData(){
     return {
